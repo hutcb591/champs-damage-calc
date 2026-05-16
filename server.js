14f10b0d6a70a7c2a5fff65c3f913ff73f1eea7f
hutcb591 <brett.hutchinson22@icloud.com>
2026-05-16 09:48:22 -0600

    fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const calc = require("@smogon/calc");
 const app = express();
-app.listen(3030, () => {
-	console.log("Server running on port 3030");
+app.listen(3000, () => {
+	console.log("Server running on port 3000");
 });
 
 // parse application/json
